8eef856156009ce5bb7060f71c7c93119774f119
dinaalamin00 <dinaalamin00@gmail.com>
2024-12-01 20:23:51 +0400

    ex00_cpp00

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diahmed <diahmed@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 19:53:43 by diahmed           #+#    #+#             */
+/*   Updated: 2024/12/01 20:22:37 by diahmed          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+// # include <string>
+
+int	main(int argc, char **argv)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 1;
+	j = 0;
+	if (argc == 1)
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for(i = 1; (int)i < argc; i++)
+	{
+		for (j = 0; j < strlen(argv[i]); j++)
+        	argv[i][j] = toupper(argv[i][j]);
+		
+	}
+	i = 1;
+	for (int i = 1; (int)i < argc; i++)
+		std::cout<<argv[i];
+	std::cout<<std::endl;
+	return (0);	
+}
