3790d5d2ef051853503058f71b1df856971ac964
dinaalamin00 <115608805+dinaalamin00@users.noreply.github.com>
2025-07-30 12:01:21 +0000

    08 00

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -3,22 +3,22 @@
 
 #include <iostream>
 #include <string>
-#include <exception>
+#include <stdexcept>
 #include <algorithm>
 
 
-class notFound : public std::exception
-{
-    public:
-        virtual const char* what() const throw();
-};
+// class notFound : public std::exception
+// {
+//     public:
+//         const char* what() const throw();
+// };
 
 template <typename T>
 typename T::const_iterator    easyfind(const T& container, int value)
 {
     typename T::const_iterator it = std::find(container.begin(), container.end(), value);
     if (it == container.end())
-        throw notFound();
+        throw std::runtime_error("not found");
     return it;
 }
 
